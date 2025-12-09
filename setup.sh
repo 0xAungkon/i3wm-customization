a854d76a19123b14c4b1f8575ca5f58226703768
0xaungkon <aungkonmalakar@gmail.com>
2025-12-09 13:49:18 +0600

    Add blueman-applet to i3 config and include necessary packages in setup script

--- setup.sh
+++ setup.sh
@@ -54,3 +54,26 @@ find ~/.config/i3 -type f -name "*.sh" -exec chmod +x {} \;
 find ~/.config/i3 -type f -name "*.py" -exec chmod +x {} \;
 chmod -R +x ~/.config/i3/config
 ######### Make pre-boot.sh Executable ##################
+
+
+read -p "Do you want to install theme? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git clone --depth 1 https://github.com/vinceliuice/WhiteSur-gtk-theme /tmp/WhiteSur-gtk-theme
+    git clone --depth 1 https://github.com/vinceliuice/WhiteSur-icon-theme /tmp/WhiteSur-gtk-icon
+    echo "✔ Themes cloned to /tmp"
+    sudo /tmp/WhiteSur-gtk-theme/install.sh
+    sudo  /tmp/WhiteSur-gtk-icon/install.sh
+    echo "✔ Themes installed successfully."
+    
+fi
+######### Install Theme ##################
+
+read -p "Do you want to configure-network? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo cp /etc/network/interfaces /etc/network/interfaces.bak
+    sudo sed -i 's/^iface wlp0s20f3.*/#&/' /etc/network/interfaces && sudo sed -i 's/wpa-ssid.*/#&/' /etc/network/interfaces && sudo sed -i 's/wpa-psk.*/#&/' /etc/network/interfaces  && sudo sed -i 's/allow-hotplug.*/#&/' /etc/network/interfaces 
+    sudo systemctl enable --now NetworkManager
+    sudo nmcli radio wifi on
+fi
